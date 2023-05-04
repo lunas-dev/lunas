2a147acae10342e2249d62881a116f8d478656b3
s19514tt <s19514tt@sfc.keio.ac.jp>
2023-05-05 01:39:22 +0900

    added simple parser

--- crates/blve_parser/src/main.rs
+++ crates/blve_parser/src/main.rs
@@ -1,3 +1,19 @@
+mod parser;
+use parser::parse;
+
 fn main() {
-    println!("Hello, world!");
+    let a = parse(
+        r#"@input(arg1:hello) "hey"
+html:
+    <div>
+
+    <h1>
+    Hello World
+    </h1>
+    </div>"#,
+    );
+    match a {
+        Ok((_, b)) => println!("{:?}", b),
+        Err(e) => println!("{:?}", e),
+    }
 }
