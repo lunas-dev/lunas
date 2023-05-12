817b04ebb6cf4cbfd75ae86b04c1e1798f2ace2d
s19514tt <s19514tt@sfc.keio.ac.jp>
2023-05-13 00:50:56 +0900

    parse by files

--- crates/blve_parser/src/main.rs
+++ crates/blve_parser/src/main.rs
@@ -6,28 +6,10 @@ use parse2::parse2;
 use parser1::parse1;
 
 fn main() {
-    let a = parse1(
-        r#"
+    // load ./sample/1.blv
+    let input = std::fs::read_to_string("./sample/1.blv").unwrap();
 
-
-@input message: string = "hey"
-@input optional:string?
-@use MyComponent from './components/my-component.blv'
-
-html:
-    <div>
-
-    <h1>
-    Hello World
-    </h1>
-    </div>
-script:
-    let hw = "hello world!"
-@input finalInput:String = "final input"
-
-
-"#,
-    );
+    let a = parse1(input.as_str());
 
     match a {
         Ok((_, b)) => {
@@ -45,56 +27,5 @@ script:
         }
         Err(_) => println!("err"),
     };
-
-    // let mut list_of_language_block = Vec::new();
-    // match a {
-    //     Ok((_, b)) => {
-    //         // pretty print the parsed items
-    //         // println!("{:?}", b)}
-    //         for i in &b {
-    //             println!("{:?}", i)
-    //         }
-
-    //         for i in b {
-    //             match i {
-    //                 LanguageBlock(lb) => {
-    //                     list_of_language_block.push(lb);
-    //                 }
-    //                 _ => {}
-    //             }
-    //         }
-    //     }
-    //     Err(e) => println!("{:?}", e),
-    // }
-
-    // let mut lang_map = HashMap::new();
-
-    // for item in list_of_language_block {
-    //     let lang_name = item.language_name;
-    //     if lang_name != "html" && lang_name != "script" {
-    //         println!("Error: Language name not recognized");
-    //         break;
-    //     }
-    //     let lang_content = item.content;
-    //     if lang_map.contains_key(&lang_name) {
-    //         println!("Error: Language already declared");
-    //         break;
-    //     }
-    //     lang_map.insert(lang_name, lang_content);
-    // }
 }
 
-/* @state(key:"hey") myState:AppState
-@input message = "hey"
-@input optional:string?
-@use MyComponent from './components/my-component.blv'
-
-@input(param:hey) optional?
-html:
-    <div>
-
-    <h1>
-    Hello World
-    </h1>
-    </div>
-@input finalInput = "final input" */
