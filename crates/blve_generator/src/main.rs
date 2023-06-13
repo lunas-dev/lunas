4213907a762d258713baee551170a24c21722c86
s19514tt <s19514tt@sfc.keio.ac.jp>
2023-06-13 16:36:39 +0900

    added tester samples

--- crates/blve_generator/src/main.rs
+++ crates/blve_generator/src/main.rs
@@ -1,56 +1,16 @@
 mod generate_js;
 mod structs;
 mod transformers;
+use std::fs;
+
 use crate::generate_js::generate_js_from_blocks;
 use blve_parser::parse_blve_file;
 
 fn main() {
-    // "Hello, world!".to_string()
-
-//     let a = r#"
-// html:
-//   <h1 id='abc'>Hello Blve!</h1>
-//   <div >${count}</div>
-//   <button @click="increment">+1</button>
-//   <button @click="clear">${interval==null?"start":"clear"}</button>
-// script:
-//   let count = 0
-//   function increment(){
-//     count++
-//     console.log(count)
-//   }
-//   function clear(){
-//     if(interval){
-//       clearInterval(interval)
-//       interval = null
-//     }else{
-//       interval = setInterval(increment, 2000)
-//     }
-//   }
-//   let interval = setInterval(increment, 2000)
-// style:
-//   h1 {
-//     color: blue;
-//   }
-//   * {
-//     font-family: 'Noto Sans', sans-serif;
-//   }
-
-// "#;
-    let a = r#"
-html:
-  <h1 id='abc'>Hello Blve!</h1>
-  <input :value="count"></input>
-  <div>${count}</div>
-script:
-  let count = 0
-  function increment(){
-    count++
-    console.log(count)
-  }
-"#;
-
-    let b = parse_blve_file(a).unwrap();
+    // get the file path from the command line
+    let args: Vec<String> = std::env::args().collect();
+    let a = fs::read_to_string(format!("./sample/{}.blv", args[1])).unwrap();
+    let b = parse_blve_file(a.as_str()).unwrap();
     let code = generate_js_from_blocks(&b);
     println!("{}", code.0);
 }
